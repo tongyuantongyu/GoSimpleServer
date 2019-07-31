8fce6535959ddc3544b3c24e268e5ff77732783d
Yuan Tong <tongyuantongyu@gmail.com>
2019-07-31 22:21:21 +0800

    add port specific

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
     "log"
 	"os"
     "net/http"
@@ -8,8 +9,12 @@ import (
 )
 
 func main() {
-    http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("www"))))
-    err := http.ListenAndServe(":8080", handlers.LoggingHandler(os.Stdout, http.DefaultServeMux))
+	var port string
+	fmt.Print("Type a port (2333 default): ")
+	fmt.Scanln(&port)
+	if port == "" {port = "2333"}
+    http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("file"))))
+    err := http.ListenAndServe(":" + port, handlers.LoggingHandler(os.Stdout, http.DefaultServeMux))
     if err != nil {
         log.Fatal("ListenAndServe: ", err)
     }
